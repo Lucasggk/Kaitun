221e5d500d6d1f519748c1f6d965c7801808fdae
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-06-19 12:53:05 -0300

    Update Principal.lua

--- Grow.A.Garden/Principal.lua
+++ Grow.A.Garden/Principal.lua
@@ -1,6 +1,7 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Kaitun/refs/heads/main/Grow.A.Garden/Functions.lua"))()
 
 local ms = getmoney()
+local etp
 
 task.spawn(function()
     while true do
@@ -8,3 +9,14 @@ task.spawn(function()
       task.wait(0.5)
     end
 end)
+
+if ms >= 0 and ms <= 1000 then
+etp = 1
+elseif ms >= 1001 and ms <= 10000 then
+etp = 2
+elseif ms >= 10001 and ms <= 100000 then
+etp = 3
+else 
+etp = 4
+print(etp)
+end
