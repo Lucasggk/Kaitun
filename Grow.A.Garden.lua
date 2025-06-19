c8dd1c12d34e89873e8475b0c7a304962652d364
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-06-19 10:41:11 -0300

    Update Grow.A.Garden.lua

--- Grow.A.Garden.lua
+++ Grow.A.Garden.lua
@@ -6,7 +6,6 @@ local terreno = nil
 
 -- pegar o terreno atual 
 
-
 local playerName = game.Players.LocalPlayer.Name
 local terreno = function() for _,f in ipairs(workspace.Farm:GetChildren()) do local d=f:FindFirstChild("Important") and f.Important:FindFirstChild("Data") if d and d:FindFirstChild("Owner") and d.Owner.Value==playerName then return f end end end()
 local ndt = terreno.Important.Data.Farm_Number.Value
