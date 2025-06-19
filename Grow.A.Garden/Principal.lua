b135ae251d6db4162c1bdc85364336d0f4e3bdeb
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-06-19 12:44:27 -0300

    Update Principal.lua

--- Grow.A.Garden/Principal.lua
+++ Grow.A.Garden/Principal.lua
@@ -1,2 +1,11 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Kaitun/refs/heads/main/Grow.A.Garden/Functions.lua"))()
 
+local ms = getMoney()
+
+task.spawn(function()
+    while true do
+      ms = getMoney()
+      print(ms)
+      task.wait(0.5)
+    end
+end)
