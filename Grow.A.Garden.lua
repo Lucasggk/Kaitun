ac8fb008a7f20d147ce8e5acdc0b40d6eed647d9
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-06-19 10:50:03 -0300

    Update Grow.A.Garden.lua

--- Grow.A.Garden.lua
+++ Grow.A.Garden.lua
@@ -1,18 +1,1 @@
--- valores
-
-local money = 0
-local etapa = 0
-
--- pegar o terreno atual 
-
-local playerName = game.Players.LocalPlayer.Name
-local terreno = (function() for _,f in ipairs(workspace.Farm:GetChildren()) do local d=f:FindFirstChild("Important") and f.Important:FindFirstChild("Data") if d and d:FindFirstChild("Owner") and d.Owner.Value==playerName then return f end end end)()
-
-local ndt = nil
-if terreno then ndt = terreno.Important.Data.Farm_Number.Value end
-
-local v3t = (function() local p=game.Players.LocalPlayer.Name for _,f in ipairs(workspace.Farm:GetChildren()) do local d=f:FindFirstChild("Important") and f.Important:FindFirstChild("Data") if d and d:FindFirstChild("Owner") and d.Owner.Value==p then return f:GetPivot().Position end end end)()
-
--- Funções 
-
-function tp(x) local char=game.Players.LocalPlayer.Character if char and char:FindFirstChild("HumanoidRootPart") then char.HumanoidRootPart.CFrame=CFrame.new(x) end end
+local money =
