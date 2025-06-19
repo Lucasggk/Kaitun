0d5d6b5c2ee56130f280871f95d55b58b4d638b4
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-06-19 14:02:43 -0300

    Update Principal.lua

--- Grow.A.Garden/Principal.lua
+++ Grow.A.Garden/Principal.lua
@@ -1,4 +1,5 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Kaitun/refs/heads/main/Grow.A.Garden/Functions.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Kaitun/refs/heads/main/Grow.A.Garden/Buysys.lua"))()
 
 local ms = tonumber(getmoney())
 local etp
