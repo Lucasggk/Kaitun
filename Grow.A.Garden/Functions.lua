38171b998dcfc8c59dcd47f2611f13edabf8b6bc
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-06-19 12:02:04 -0300

    Update Functions.lua

--- Grow.A.Garden/Functions.lua
+++ Grow.A.Garden/Functions.lua
@@ -18,20 +18,20 @@ function getFarmPos()
 end
 
 function getFarmNumber()
-    local p=game.Players.LocalPlayer.Name
-    for _,f in pairs(workspace:WaitForChild("Farm"):GetChildren()) do
-        local d=f:FindFirstChild("Important") and f.Important:FindFirstChild("Data")
-        if d and d:FindFirstChild("Owner") and d.Owner.Value==p then
+    local p = game.Players.LocalPlayer.Name
+    for _, f in pairs(workspace:WaitForChild("Farm"):GetChildren()) do
+        local d = f:FindFirstChild("Important") and f.Important:FindFirstChild("Data")
+        if d and d:FindFirstChild("Owner") and d.Owner.Value == p then
             return d:FindFirstChild("Farm_Number") and d.Farm_Number.Value
         end
     end
 end
 
 function tp(x, y)
-    y = y or Vector3.new(0,5,0)
+    y = y or Vector3.new(0, 5, 0)
     local h = game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
     if h then h.CFrame = CFrame.new(x + y) end
 end
 
 print(getFarmPos())
-tp(getFarmPos(), Vector3.new(0, 0, 0)
+tp(getFarmPos(), Vector3.new(0, 0, 0))
