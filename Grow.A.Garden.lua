5e419e9adcdb6b151907d4ada82b6fa59856ab98
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-06-19 10:45:36 -0300

    Update Grow.A.Garden.lua

--- Grow.A.Garden.lua
+++ Grow.A.Garden.lua
@@ -1,23 +1,18 @@
 -- valores
 
-local money =
+local money = 0
 local etapa = 0
-local terreno = nil
 
 -- pegar o terreno atual 
 
 local playerName = game.Players.LocalPlayer.Name
-local terreno = function() for _,f in ipairs(workspace.Farm:GetChildren()) do local d=f:FindFirstChild("Important") and f.Important:FindFirstChild("Data") if d and d:FindFirstChild("Owner") and d.Owner.Value==playerName then return f end end end()
-local ndt = terreno.Important.Data.Farm_Number.Value
-local v3t = function() local p=game.Players.LocalPlayer.Name for _,f in ipairs(workspace.Farm:GetChildren()) do local d=f:FindFirstChild("Important") and f.Important:FindFirstChild("Data") if d and d:FindFirstChild("Owner") and d.Owner.Value==p then return f:GetPivot().Position end end end()
+local terreno = (function() for _,f in ipairs(workspace.Farm:GetChildren()) do local d=f:FindFirstChild("Important") and f.Important:FindFirstChild("Data") if d and d:FindFirstChild("Owner") and d.Owner.Value==playerName then return f end end end)()
+
+local ndt = nil
+if terreno then ndt = terreno.Important.Data.Farm_Number.Value end
+
+local v3t = (function() local p=game.Players.LocalPlayer.Name for _,f in ipairs(workspace.Farm:GetChildren()) do local d=f:FindFirstChild("Important") and f.Important:FindFirstChild("Data") if d and d:FindFirstChild("Owner") and d.Owner.Value==p then return f:GetPivot().Position end end end)()
 
 -- Funções 
 
-function tp(x)
-	local char = game.Players.LocalPlayer.Character
-	if char and char:FindFirstChild("HumanoidRootPart") then
-		char.HumanoidRootPart.CFrame = CFrame.new(x)
-	end
-end
-
--- 
+function tp(x) local char=game.Players.LocalPlayer.Character if char and char:FindFirstChild("HumanoidRootPart") then char.HumanoidRootPart.CFrame=CFrame.new(x) end end
